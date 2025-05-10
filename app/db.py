c29e7e4745863ece9bcd562f407897c44df8adf6
OwenGN <gumaneitwe99@gmail.com>
2025-05-09 23:02:27 -0700

    more backend and database

--- app/db.py
+++ app/db.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 import os
+from dotenv import load_dotenv
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://guman:Jnkjuliet_99@localhost:5432/dynamic_wallet")
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
-Session = sessionmaker(bind=engine)
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
